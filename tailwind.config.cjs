8d33423c6ac8450513fe6f4ae31cf6ff2759a274
Gianmarco <cavallogianmarco@gmail.com>
2023-08-16 22:43:53 +0200

    cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["CabinetGrotesk", "Satoshi", ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateRows: {
+        "auto-250": "repeat(auto-fill, 250px)",
+      },
+      gridTemplateColumns: {
+        "4-minmax": "repeat(4, minmax(150px, 1fr))",
+      },
       colors: {
         darkslate: {
-          100: "#5B5B5B",
-          200: "#505050",
-          300: "#454545",
-          400: "#3A3A3A",
-          500: "#2F2F2F",
-          600: "#242424",
-          700: "#191919",
+          50: "#3D3D3D",
+          100: "#2C2C2C",
+          200: "#262626",
+          300: "#202020",
+          400: "#1A1A1A",
+          500: "#171717" /* Exactly your example for the background */,
+          600: "#141414",
+          700: "#111111",
           800: "#0E0E0E",
-          900: "#030303",
+          900: "#0B0B0B" /* Deeper and darker */,
         },
         primary: {
           100: "#F9CDD3",
