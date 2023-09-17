b724a0f17c8317fe716e064d0799745ea1d21fce
Gianmarco <cavallogianmarco@gmail.com>
2023-09-17 16:13:02 +0200

    tailwind -> unocss and some other changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gianmarco.xyz/",
-  integrations: [tailwind(), sitemap(), robotsTxt({
-    sitemap: ["https://gianmarco.xyz/sitemap-index.xml", "https://gianmarco.xyz/sitemap-0.xml"]
-  }), solidJs()],
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://gianmarco.xyz/sitemap-index.xml",
+        "https://gianmarco.xyz/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
---
+++ uno.config.ts
@@ -0,0 +1,67 @@
+// uno.config.ts
+import { defineConfig, presetUno, presetWebFonts } from "unocss";
+
+export default defineConfig({
+  theme: {
+    boxShadow: {
+      custom: `2px 2px theme("colors.primary.500")`,
+      "custom-hover": `1px 1px theme("colors.primary.500")`,
+    },
+    fontFamily: {
+      sans: ["CabinetGrotesk", "Satoshi"],
+    },
+    gridTemplateRows: {
+      "auto-250": "repeat(auto-fill, 250px)",
+    },
+    gridTemplateColumns: {
+      "4-minmax": "repeat(4, minmax(150px, 1fr))",
+    },
+    colors: {
+      gray: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#E5E5E5",
+        300: "#D4D4D4",
+        400: "#A3A3A3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+      darkslate: {
+        50: "#3D3D3D",
+        100: "#2C2C2C",
+        200: "#262626",
+        300: "#202020",
+        400: "#1A1A1A",
+        500: "#171717" /* Exactly your example for the background */,
+        600: "#141414",
+        700: "#111111",
+        800: "#0E0E0E",
+        900: "#0B0B0B" /* Deeper and darker */,
+      },
+      primary: {
+        100: "#F9CDD3",
+        200: "#F3A3AA",
+        300: "#EC7981",
+        400: "#E64F59",
+        500: "#E63946",
+        600: "#CF2F3D",
+        700: "#B82534",
+        800: "#A01B2B",
+        900: "#891321",
+      },
+    },
+  },
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "fontshare",
+      fonts: {
+        sans: ["Cabinet Grotesk", "Satoshi"],
+        serif: "Zodiak",
+      },
+    }),
+  ],
+});
