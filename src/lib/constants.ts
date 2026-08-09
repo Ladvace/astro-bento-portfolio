1cee962c9670b644d3b846350d5e659e09b4eb01
ladvace <cavallogianmarco@gmail.com>
2026-08-09 08:42:06 +0200

    refactor: remove dead components, props and the constants alias

--- src/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-import { SITE } from "../site-config";
-
-export const LINKS = SITE.links;
-
-export const loaderAnimation = [
-  ".loader",
-  { opacity: [1, 0], pointerEvents: "none" },
-  { ease: "easeOut" },
-];
