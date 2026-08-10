6ef23b9844402cfa11ab6bac15d780920bd3f81b
ladvace <cavallogianmarco@gmail.com>
2026-08-10 21:10:08 +0200

    refactor: remove unused blog posts and Dribbble link

--- src/data/blog/post1.md
+++
@@ -1,9 +0,0 @@
----
-title: "How I made 4.5 Millions at 24 years old"
-description: "How I made 4.5 Millions at 24 years old"
-pubDate: 2024-03-26
-category: "intro"
-draft: false
----
-
-# I didn't
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -16,9 +16,7 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       content: sanitizeHtml(parser.render(post.body)),
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
 }
